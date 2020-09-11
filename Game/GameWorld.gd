8218e902ecc1f2c7a025fa2ecf825878c9e5f780
Jianan Ye <jianan@web.de>
2020-09-11 19:30:51 +0200

    start pixel_to_hex

--- Game/GameWorld.gd
+++ Game/GameWorld.gd
@@ -74,13 +74,51 @@ func get_random_tile_simplex(noise_value:float) -> int:
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.pressed and not $CanvasLayer/TileView.visible:
+		if event.pressed and not $CanvasLayer/TileView.visible and Input.is_action_pressed("main_click"):
 			var pos = event.position
 			var camera_pos = $KinematicBody2D/CollisionShape2D/Camera2D.position
-			var tile_pos = _tilemap.world_to_map(pos) - _tilemap.world_to_map(camera_pos)
+			var tile_pos = pixel_to_pointy_hex(pos) # - pixel_to_pointy_hex(camera_pos)
 			var tile_image = _tilemap.get_cell(tile_pos.x, tile_pos.y)
-			print(tile_pos)
-			print(tile_image)
+			print("tile_pos",tile_pos)
 			$CanvasLayer/TileView.visible = true
 			$CanvasLayer/TileView/Background/Content/Body/TileInfo/TilePosition.text = str(tile_pos.x) + ", " + str(tile_pos.y)
 			#$TileView/TextureRect/VBoxContainer/HBoxContainer/VBoxContainer/TextureRect.texture = 
+
+
+
+func pixel_to_pointy_hex(point):
+	var q = (sqrt(3)/3 * point.x  -  1/3 * point.y) / _tilemap.cell_size.x
+	var r = (2/3 * point.y) / _tilemap.cell_size.y
+	print("point ", point, "hex ", q ,r, (2/3 * point.y))
+	return Vector2(q,r)
+	
+func hex_round(hex):
+	return cube_to_axial(cube_round(axial_to_cube(hex)))
+	
+func cube_round(cube):
+	var rx = round(cube.x)
+	var ry = round(cube.y)
+	var rz = round(cube.z)
+	
+	var x_diff = abs(rx - cube.x)
+	var y_diff = abs(ry - cube.y)
+	var z_diff = abs(rz - cube.z)
+
+	if x_diff > y_diff and x_diff > z_diff:
+		rx = -ry-rz
+	else: if y_diff > z_diff:
+			ry = -rx-rz
+	else:
+		rz = -rx-ry
+	return Vector3(rx, ry, rz)
+
+func cube_to_axial(cube):
+	var q = cube.x
+	var r = cube.z
+	return Vector2(q, r)
+	
+func axial_to_cube(hex):
+	var x = hex.q
+	var z = hex.r
+	var y = -x-z
+	return Vector3(x, y, z)
